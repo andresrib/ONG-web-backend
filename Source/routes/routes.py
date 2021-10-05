aa03507c42086bb62c633844ef02598dda0bdb7c
andresrib <andresrib3@gmail.com>
2021-10-05 19:55:57 -0300

    fix allow origin

--- Source/routes/routes.py
+++ Source/routes/routes.py
@@ -5,8 +5,21 @@ from pathlib import Path
 sys.path.append(os.path.abspath(Path(os.getcwd()) / ".." ))
 from routes.user import user_router
 from routes.login import login_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 print(os.getcwd())
 
