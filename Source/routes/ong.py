909fa17c7ed28f215299ff9612085434ec113c2b
andresrib <andresrib3@gmail.com>
2021-11-29 15:38:28 -0300

    feat social action routes

--- Source/routes/ong.py
+++ Source/routes/ong.py
@@ -4,49 +4,53 @@ import sys
 from pathlib import Path
 from sqlalchemy.orm import Session
 sys.path.append(os.path.abspath(Path(os.getcwd()) / ".." ))
-from schemas.ong import OngPost
 from database.database import get_db
-from crud.ong import insert_ong, retrieve_ong, retrieve_ong_by_user,update_ong, delete_ong
+from schemas.social_action import SocialActionPost
+from crud.social_action import *
 from crud.login import oauth2_scheme, get_current_user_from_token, retrieve_login_information
 
-ong_router = APIRouter()
+action_router = APIRouter()
 
-@ong_router.post("/ong")
-async def create_ong(
+@action_router.post("/action")
+async def create_action(
     response: Response,
-    ong: OngPost,
+    action: SocialActionPost,
     db: Session = Depends(get_db),
     token: str = Depends(oauth2_scheme)
 ):
     email = await get_current_user_from_token(token)
     user = await retrieve_login_information(db, email)
-    return insert_ong(db, ong, user.user_id)
+    return insert_social_action(db, action, user.user_id)
 
-@ong_router.get("/ong/{id}")
-async def get_ong(
+@action_router.get("/action/{id}")
+async def get_action(
     id: str,
     db: Session = Depends(get_db),
     token: str = Depends(oauth2_scheme)
 ):
-    if id == "user":
-        email = await get_current_user_from_token(token)
-        user = await retrieve_login_information(db, email)  
-        return retrieve_ong_by_user(db, user.user_id)
-    return retrieve_ong(db, id)
+    return retrieve_social_action(db, id)
 
-@ong_router.put("/ong/{id}")
-async def put_ong(
-    ong: OngPost,
+@action_router.get("/ong/action/{id}")
+async def get_action(
+    id: str,
+    db: Session = Depends(get_db),
+    token: str = Depends(oauth2_scheme)
+):
+    return retrieve_social_action_by_ong(db, id)
+
+@action_router.put("/action/{id}")
+async def put_action(
+    action: SocialActionPost,
     id: str,
     db: Session = Depends(get_db),
     token: str = Depends(oauth2_scheme)
 ): 
-    return update_ong(db, ong, id)
+    return update_social_action(db, action, id)
 
-@ong_router.delete("/ong")
-async def dlt_ong(
+@action_router.delete("/action")
+async def dlt_action(
     id: str,
     db: Session = Depends(get_db),
     token: str = Depends(oauth2_scheme)
 ):
-    return delete_ong(db, id)
+    return delete_social_action(db, id)
